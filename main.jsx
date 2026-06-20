0aa799e8da06fd8d158a4c0bb1f4d360520efee1
KD76V <kvd5006@gmail.com>
2026-06-20 10:31:54 +0530

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import VadvalaApp from './VadvalaApp.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <VadvalaApp />
-  </React.StrictMode>,
-)
